a5846602d0b400a54e4b8123bf2fbed5be3c5245
meryemabdedou83-ux <meryemabdedou83@gmail.com>
2026-03-08 23:38:32 -0400

    Création des instances Employe et Voiture

--- attellier 4.py
+++ attellier 4.py
@@ -18,3 +18,12 @@ class Voiture:
 
     def afficherInformations(self):
         print(self.marque, self.matricule, self.annee, self.kilometrage)
+
+        from employe import Employe
+        from voiture import Voiture
+
+        e1 = Employe("A123", "Ali", "Samir")
+        e2 = Employe("B456", "Sara", "Nadia")
+
+        v1 = Voiture("123ABC", 2020, "Toyota", 10000)
+        v2 = Voiture("456DEF", 2019, "Honda", 20000)
